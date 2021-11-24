fe2d0cd919a60c8f67ae9e53695c7ae71aaaaf54
Pitt Wu <kgb00128@gmail.com>
2021-11-24 17:30:29 +0800

    feat: test get method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const app = express();
 
 const PORT = 3002;
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello Node.js!</h1>`);
+});
+
+app.get("/home", (req, res) => {
+  res.send(`<h1>This is Homepage.</h1>`);
+});
+
 app.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
 });
