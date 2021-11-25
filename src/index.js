3e0cf1c858c0fcdf1987054eded9b0eb2b88dd75
Pitt Wu <kgb00128@gmail.com>
2021-11-25 17:39:54 +0800

    feat: test bmi calculator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 3002;
 
@@ -7,8 +10,17 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-app.get("/home", (req, res) => {
-  res.send(`<h1>This is Homepage.</h1>`);
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(`${__dirname}/bmiCalculator.html`);
+});
+
+app.post("/bmiCalculator", (req, res) => {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height) / 100;
+  let heightPow = Math.pow(height, 2);
+
+  const result = weight / heightPow;
+  res.send(`<h1>BMI : ${result}</h1>`);
 });
 
 app.listen(PORT, () => {
