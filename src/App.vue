fa5b101450c1c90e80a1ee31c4e824ecb0155fae
Pitt Wu <kgb00128@gmail.com>
2024-12-08 08:52:02 +0800

    [Chore] init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PixiTest from "./components/PixiTest.vue";
+</script>
+
+<template>
+  <main>
+    <PixiTest />
+  </main>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/PixiTest.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import * as PIXI from "pixi.js";
+</script>
+
+<template></template>
+
+<style scoped></style>
